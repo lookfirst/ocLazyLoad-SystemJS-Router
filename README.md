58f7a3fe434fbc0fd85dfa372d8a0091838d95ce
Jon Stevens <latchkey@gmail.com>
2015-03-10 16:44:15 -0700

    initial add

---
+++ README.md
@@ -0,0 +1,5 @@
+# ocLayzLoad-SystemJS-Router
+
+This class combines three different projects to enable route based lazy loading of angular modules.
+
+Please see the [SystemJS Seed](https://github.com/lookfirst/systemjs-seed/) project for details on usage.
---
+++ index.js
@@ -0,0 +1,46 @@
+import 'angular-ui-router';
+import 'ui-router-extras/release/modular/ct-ui-router-extras.core';
+import 'ui-router-extras/release/modular/ct-ui-router-extras.future';
+import 'oclazyload';
+
+export default function(angularModule, futureRoutes) {
+
+	angularModule.requires.push('ui.router');
+	angularModule.requires.push('ct.ui.router.extras.core');
+	angularModule.requires.push('ct.ui.router.extras.future');
+	angularModule.requires.push('oc.lazyLoad');
+
+	var RouterConfig = ['$ocLazyLoadProvider', '$stateProvider', '$futureStateProvider',
+		function($ocLazyLoadProvider, $stateProvider, $futureStateProvider) {
+
+			$futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState',
+				function($q, $ocLazyLoad, futureState) {
+
+					var def = $q.defer();
+
+					System.import(futureState.src).then(loaded => {
+						var newModule = loaded;
+						if (!loaded.name) {
+							var key = Object.keys(loaded);
+							newModule = loaded[key[0]];
+						}
+
+						$ocLazyLoad.load(newModule).then(function() {
+							def.resolve();
+						}, function(err) {
+							throw err;
+						});
+					});
+
+					return def.promise;
+				}
+			]);
+
+			futureRoutes.forEach(function(r) {
+				$futureStateProvider.futureState(r);
+			});
+		}
+	];
+
+	return RouterConfig;
+}
